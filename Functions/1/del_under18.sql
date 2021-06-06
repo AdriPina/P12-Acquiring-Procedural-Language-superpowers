a05798f97161f29b0b3c687a6a0723ada697caef
AdriPina <apina2@cifpfbmoll.eu>
2021-06-06 22:42:24 +0200

    delete function 1

--- Functions/1/del_under18.sql
+++
@@ -1,9 +0,0 @@
-create function public.del_under18() returns SETOF integer
-	language sql
-as $$
-delete from customers where age<18
-returning customerid;
-$$;
-
-alter function public.del_under18() owner to postgres;
-
